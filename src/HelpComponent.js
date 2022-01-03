3f89a04efe7e7399b4f4212f7cc2ca8ed664876f
TreyDettmer <dettmertrey@gmail.com>
2022-01-02 20:14:19 -0800

    removed ssl certificate used for https during development

--- src/HelpComponent.js
+++ src/HelpComponent.js
@@ -10,7 +10,7 @@ export default function HelpComponent({displaySettings}) {
                 <li>Unmute the sound and ensure that the mobile device isn't silented in order to hear the Heads Up audio cues.</li>
                 <li>To exit out of a round, refresh the webpage.</li>
                 <li>There are no teams or scorekeeping in this version of Catch Phrase.</li>
-                <li>In order for the app to be able to ask permission to use the mobile device's gyroscope, ensure that https is used in the url, not http.</li>
+                <li><b>In order for the app to be able to ask permission to use the mobile device's gyroscope, ensure that https is used in the url, not http.</b></li>
                 <li><a href='https://howdoyouplayit.com/catchphrase-rules-play-catchphrase/'>Catch Phrase rules</a></li>
                 <li><a href='https://www.wikihow.com/Play-Heads-Up!'>Heads Up rules</a></li>
                 
